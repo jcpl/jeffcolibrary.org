47fae5c7457b733ac5675ff62a21118f916ca525
Carl Wiedemann <carl.wiedemann@gmail.com>
2013-01-14 18:19:17 -0500

    Add feeds in deployment script

--- sites/all/scripts/deploy-feature--jcplfeeds.php
+++ sites/all/scripts/deploy-feature--jcplfeeds.php
@@ -23,4 +23,30 @@ if (PHP_SAPI == 'cli') {
     ->condition('module', 'views')
     ->condition('theme', 'precipice')
     ->execute();
+
+  // Add each feed item, added by root.
+  $acct = user_load(1);
+  foreach (array(
+    'http://sable.jefferson.lib.co.us/feeds/adultbooks.xml',
+    'http://sable.jefferson.lib.co.us/feeds/featurefilm.xml',
+    'http://sable.jefferson.lib.co.us/feeds/allmusic.xml',
+    'http://sable.jefferson.lib.co.us/feeds/yabooks.xml',
+    'http://sable.jefferson.lib.co.us/feeds/jthingsthatgo.xml'
+  ) as $feed_url) {
+    // Add some feeds.
+    $node = (object) array(
+      'uid' => $acct->uid,
+      'name' => (isset($acct->name) ? $acct->name : ''),
+      'type' => 'jcpl_sable_feed',
+      'language' => LANGUAGE_NONE,
+      'feeds' => array(
+        'FeedsHTTPFetcher' => array(
+          'source' => $feed_url
+        )
+      )
+    );
+    node_save($node);
+    drupal_set_message(t('@feed_url maps to node !nid', array('@feed_url' => $feed_url, '!nid' => $node->nid)));
+  }
+
 }
