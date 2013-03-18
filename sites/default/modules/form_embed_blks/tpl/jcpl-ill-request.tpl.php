742b3a796dce51144412b75a65f695549c6c6cde
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-03-18 16:33:30 -0600

    New module form_embed_blks set up to embed script and code for Suggest a title, Request ILL, Contact us.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-ill-request.tpl.php
@@ -0,0 +1,10 @@
+<div>
+<a name="form1162141609" id="formAnchor1162141609"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?1162141609"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscVw2IigqdAOws&1162141609",
+	width: "100%"
+});
+</script>
+</div>
---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-suggest-title.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form734772012" id="formAnchor734772012"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?734772012"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV%2BJger61bVEG&734772012",
+	width: "100%"
+});
+</script>
+</div>
