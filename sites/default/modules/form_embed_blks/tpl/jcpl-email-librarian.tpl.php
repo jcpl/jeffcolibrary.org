941324fed1a4a0985461a3893c21bdfc01a41dbc
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-03-19 12:53:36 -0600

    Added email a librarian form code to form_embed_blks.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-email-librarian.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form721943922" id="formAnchor721943922"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?721943922"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscVz13lr5WBGgw&721943922",
+	width: "100%"
+});
+</script>
+</div>
