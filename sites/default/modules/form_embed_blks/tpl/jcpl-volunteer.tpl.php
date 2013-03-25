5ee42c7033121c671d928a58b32a9eb86c3a52b7
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-03-25 14:36:43 -0600

    Added volunteer form to form_embed_blks.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-volunteer.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form484543304" id="formAnchor484543304"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?484543304"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV75i%2FnIiidxM&484543304",
+	width: "100%"
+});
+</script>
+</div>
