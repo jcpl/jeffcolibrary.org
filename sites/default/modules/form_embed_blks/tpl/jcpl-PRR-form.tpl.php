930a32fa09375d6978023dc0a605a9760a05902f
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-02 07:43:39 -0600

    Edit file name for jcpl-PRR-form.tpl

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-PRR-form.tpl.php
@@ -0,0 +1,9 @@
+<a name="form757529137" id="formAnchor757529137"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?757529137"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV9D%2BJrUQD2yW&757529137",
+	width: "100%"
+});
+</script>
+
