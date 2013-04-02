b95a683d44e4e3a9c4376688e6c828bacb0484e7
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-02 05:54:00 -0600

    We Recommend Nextreads lists and subscribe tpl blocks in jeffcolibrary_blks.

---
+++ sites/default/modules/jeffcolibrary_blks/tpl/jcpl-nextreads-lists.tpl.php
@@ -0,0 +1,113 @@
+<div><a href="books-movies-music/we-recommend/subscribe-Nextreads">Subsciribe to receive recommendations</a></div>
+<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="This table is used for layout"><tr valign="top" align="left">
+<td>
+<p style="text-indent:3px"><b>Armchair Travel</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1205&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Audiobooks</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1211&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Biography and Memoir</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1203&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Business and Personal Finance</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1206&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Christian Fiction</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1196&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Fantasy</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1199&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Fiction A-Z</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1194&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Historical Fiction</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1198&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>History and Current Events</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1204&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Home, Garden and Do-It-Yourself</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1202&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Horror</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1197&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<!-- <p style="text-indent:3px"><b>JCPL Special Booklists</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=2486&Max=2&FMT=1&Sort=1"></script>
+</p> -->
+
+<p style="text-indent:3px"><b>Kids' Books</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=6638&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Mind and Body Fitness</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1201&Max=2&FMT=3&Sort=1"></script>
+</p>
+</td>
+
+
+
+<td>
+<p style="text-indent:3px"><b>Mystery</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1192&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Nature and Science</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1209&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>New York Times Fiction Bestsellers</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=11967&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>New York Times Non-Fiction Bestsellers</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=12246&Max=13&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Picture Books</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=6436&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Popular Culture</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1208&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Romance</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1195&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Science Fiction</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1200&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Spirituality and Religion</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1207&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Teen Scene</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1210&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Thrillers and Suspense</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1193&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+<p style="text-indent:3px"><b>Tween Reads</b><br />
+<script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=9641&Max=2&FMT=3&Sort=1"></script>
+</p>
+
+</td>
+</tr></table>
---
+++ sites/default/modules/jeffcolibrary_blks/tpl/jcpl-nextreads-subscribe.tpl.php
@@ -0,0 +1,21 @@
+<div class="small" style="width:77%; margin-left:50px; padding-left:1px;">
+
+Welcome to NextReads, a service brought to you through JCPL and EBSCO. Choose from one of the categories below and receive email newsletters with selected titles of great reads.  When you sign up for a newsletter you will receive a confirmation email and then you will receive the current issue of the newsletter(s) you selected.  Emails will come monthly or bi-monthly, depending on the category selected. Position the cursor on a category for a description which includes the frequency.<br /><br />
+
+<a href="books-movies-music/we-recommend">Archive of Past Newsletters</a><br /><br />
+
+<b>NOTE:</b> If you encounter display problems using the Manage Subscriptions link below, use this one instead:  <a href="javascript: window.open('http://www.nextreads.com/members.aspx?S=59','','toolbar=no,scrollbars=yes,width=650,height=520,left=0,top=0'); void('');">Manage Subscriptions</a><br /><br />
+
+<!-- Welcome to the beta test of EBSCO's Next Reads. When you sign up for a newsletter you will receive a confirmation email and then you will receive the selected newsletter. New subscribers will receive a Dec newsletter immediately, and can expect to see a January newsletter sample approx. 1/23. For more information on NextReads, go to <a href="http://www.nextreads.com">www.nextreads.com</a>.<br /><br />
+
+EBSCO thanks Jefferson County Public Library for their cooperation and willingness to participate in the beta testing of NextReads, and for sharing their catalog and expertise with us.<br /><br />
+
+Welcome to the beta test of EBSCO's Next Reads.  When you sign up for a newsletter you will receive a confirmation email and then you will receive the selected newsletter.  During the beta testing period you will be sent a November, December, and January newsletter. You will be sent a survey about each month's newsletter.<br /><br />
+
+<b>NOTE:</b> For the beta; please only subscribe to those lists that are in bold and underlined.  The other lists will be part of the final product, but are not available during the beta test.<br /> -->
+</div>
+
+<iframe frameborder="0" name="cwindow" id="cwindow" width="561" height="650" src="http://www.nextreads.com/join.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd" marginheight="10px" onLoad="javascript:parent.document.location='#'"></iframe>
+<!-- <iframe frameborder="0" name="cwindow" width="561" height="600" src="http://www.nextreads.com/join.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd" marginheight="10px"></iframe><br /> -->
+<!-- <iframe id="test" height="600" width="500" src="http://www.nextreads.com/join.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd" onLoad="javascript:parent.document.location='#'">
+</iframe> -->
