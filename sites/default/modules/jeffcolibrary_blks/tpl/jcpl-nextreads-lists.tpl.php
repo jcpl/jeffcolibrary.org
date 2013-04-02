e2df8033d99edfea04dc164ac00abce5a6c0de6d
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-02 07:14:42 -0600

    Edits to jcpl-nextreads-lists.tpl layout for Nextreads.

--- sites/default/modules/jeffcolibrary_blks/tpl/jcpl-nextreads-lists.tpl.php
+++ sites/default/modules/jeffcolibrary_blks/tpl/jcpl-nextreads-lists.tpl.php
@@ -1,10 +1,11 @@
-<div><a href="subscribe-Nextreads">Subscribe to receive lists</a></div>
+<!--<div><a href="subscribe-Nextreads">Subscribe to receive lists</a><br /><br /></div> -->
 <table width="100%" border="0" cellpadding="0" cellspacing="0" summary="This table is used for layout"><tr align="left">
 <td>
 <p style="text-indent:3px"><b>Armchair Travel</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1205&Max=2&FMT=3&Sort=1"></script>
 </p>
-
+</td>
+<td>
 <p style="text-indent:3px"><b>Audiobooks</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1211&Max=2&FMT=3&Sort=1"></script>
 </p>
@@ -15,6 +16,8 @@
 <p style="text-indent:3px"><b>Biography and Memoir</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1203&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Business and Personal Finance</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1206&Max=2&FMT=3&Sort=1"></script>
@@ -26,7 +29,8 @@
 <p style="text-indent:3px"><b>Christian Fiction</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1196&Max=2&FMT=3&Sort=1"></script>
 </p>
-
+</td>
+<td>
 <p style="text-indent:3px"><b>Fantasy</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1199&Max=2&FMT=3&Sort=1"></script>
 </p>
@@ -37,6 +41,8 @@
 <p style="text-indent:3px"><b>Fiction A-Z</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1194&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Historical Fiction</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1198&Max=2&FMT=3&Sort=1"></script>
@@ -48,6 +54,8 @@
 <p style="text-indent:3px"><b>History and Current Events</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1204&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Home, Garden and Do-It-Yourself</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1202&Max=2&FMT=3&Sort=1"></script>
@@ -59,6 +67,8 @@
 <p style="text-indent:3px"><b>Horror</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1197&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Kids' Books</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=6638&Max=2&FMT=3&Sort=1"></script>
@@ -70,6 +80,8 @@
 <p style="text-indent:3px"><b>Mind and Body Fitness</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1201&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Mystery</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1192&Max=2&FMT=3&Sort=1"></script>
@@ -81,6 +93,8 @@
 <p style="text-indent:3px"><b>Nature and Science</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1209&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>New York Times Fiction Bestsellers</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=11967&Max=2&FMT=3&Sort=1"></script>
@@ -92,6 +106,8 @@
 <p style="text-indent:3px"><b>New York Times Non-Fiction Bestsellers</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=12246&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Picture Books</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=6436&Max=2&FMT=3&Sort=1"></script>
@@ -103,6 +119,8 @@
 <p style="text-indent:3px"><b>Popular Culture</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1208&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Romance</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1195&Max=2&FMT=3&Sort=1"></script>
@@ -114,6 +132,8 @@
 <p style="text-indent:3px"><b>Science Fiction</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1200&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Spirituality and Religion</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1207&Max=2&FMT=3&Sort=1"></script>
@@ -125,6 +145,8 @@
 <p style="text-indent:3px"><b>Teen Scene</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1210&Max=2&FMT=3&Sort=1"></script>
 </p>
+</td>
+<td>
 
 <p style="text-indent:3px"><b>Thrillers and Suspense</b><br />
 <script language="javascript" src="http://www.nextreads.com/display.aspx?SID=582a9df1-001f-4661-8366-bbbd4a3e75cd&NL=1193&Max=2&FMT=3&Sort=1"></script>
