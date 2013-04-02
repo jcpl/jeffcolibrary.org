454f09ba77ea48c269c0f1a66f9b837d02ed9805
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-02 12:28:31 -0600

    Added Online Book Clubs to jeffcolibrary_blks and jcpl_online_clubs.tpl.

---
+++ sites/default/modules/jeffcolibrary_blks/tpl/jcpl-online-clubs.tpl.php
@@ -0,0 +1,97 @@
+<div style="width:75%" class="small">Join our Online Book Clubs and start reading books in your e-mail. Each day we'll send you a 5-minute portion of a book. By the end of the week, you'll have read 2-3 chapters. If you'd like to finish a book, stop by the library and pick up a copy or reserve one online. Every week we feature new books. This week's books are listed below. Sign up today and start reading tomorrow.</div><br />
+ 
+<form name="signup" action="http://www.supportlibrary.com/su/su.cfm?x=73441" method="post">
+ 
+<b>To join enter your email address:</b> <input id="email" type="text" name="email" size="40" maxlength="128"><br /><br />
+<b>Choose your book clubs:</b><br />
+ 
+<table width="100%" cellpadding="0" cellspacing="0"><tr>
+<td width="20%"><input id="List2" type="checkbox" name="List2">&nbsp;Fiction<br />
+<input id="List11" type="checkbox" name="List11">&nbsp;Pre-Publication<br /></td>
+ 
+<td width="20%"><input id="List6" type="checkbox" name="List6">&nbsp;Good News<br />
+<input id="List5" type="checkbox" name="List5">&nbsp;Romance<br /></td>
+ 
+<td width="20%"><input id="List8" type="checkbox" name="List8">&nbsp;Mystery<br />
+<input id="List7" type="checkbox" name="List7">&nbsp;Science Fiction<br /></td>
+ 
+<td width="20%"><input id="List1" type="checkbox" name="List1">&nbsp;Non-Fiction<br />
+<input id="List3" type="checkbox" name="List3">Teen<br /></td>
+ 
+<td width="20%"><input name="join" type="submit" value="JOIN NOW" ></td>
+</tr>
+</table>
+ 
+<input name="joining" type="hidden" value="joining">
+</form>
+ 
+<hr style="color:#4f669c" size="1" noshade="noshade" />
+ 
+<span style="font-size:14px;font-weight:bold">This week's books</span><br /><br />
+ 
+<table width="95%" border="0" cellpadding="0" cellspacing="0" summary="This table is used for layout"><tr valig="top" align="center" valign="bottom">
+<td width="25%">
+<b>Fiction Book Club</b>
+<a href="http://www.emailbookclub.com/book/fiction.htm"><img src="http://www.emailbookclub.com/book/fiction.jpg" border="0" alt="Book Cover" width="94" height="145" /></a><br />
+<a href="http://www.emailbookclub.com/book/fiction.htm">About this book</a><br /><br />
+</td>
+ 
+<td width="25%">
+<b>Good News Book Club</b>
+<a href="http://www.emailbookclub.com/book/goodnews.htm"><img src="http://www.emailbookclub.com/book/goodnews.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br /><a href="http://www.emailbookclub.com/book/goodnews.htm">About this book</a><br /><br />
+<br /><br />
+</td>
+ 
+<td width="25%">
+<b>Mystery Book Club</b>
+<a href="http://www.emailbookclub.com/book/mystery.htm"><img src="http://www.emailbookclub.com/book/mystery.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br />
+<a href="http://www.emailbookclub.com/book/mystery.htm">About this book</a><br /><br />
+</td>
+ 
+<td width="25%">
+<b>Non-Fiction Book Club</b>
+<a href="http://www.emailbookclub.com/book/original.htm"><img src="http://www.emailbookclub.com/book/original.jpg" border="0" alt="Book Cover" width="94" height="145" /></a><br />
+<a href="http://www.emailbookclub.com/book/original.htm">About this book</a><br /><br />
+ 
+</td>
+</tr>
+ 
+<tr>
+<td colspan="4">
+<hr style="color:#4f669c" size="1" noshade="noshade" />
+<br />
+</td></tr>
+ 
+<tr align="center" valign="bottom">
+<td>
+<b>Pre-Publication<br />
+Book Club</b>
+<a href="http://www.emailbookclub.com/book/prepub.htm"><img src="http://www.emailbookclub.com/book/prepub.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br />
+<a href="http://www.emailbookclub.com/book/prepub.htm">About this book</a><br /><br />
+</td>
+ 
+<td width="25%">
+<b>Romance Book Club</b>
+<a href="http://www.emailbookclub.com/book/romance.htm"><img src="http://www.emailbookclub.com/book/romance.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br />
+<a href="http://www.emailbookclub.com/book/romance.htm">About this book</a><br /><br />
+</td>
+ 
+<td width="25%">
+<b>Science Fiction<br />
+Book Club</b>
+<a href="http://www.emailbookclub.com/book/sf.htm"><img src="http://www.emailbookclub.com/book/sf.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br />
+<a href="http://www.emailbookclub.com/book/sf.htm">About this book</a><br /><br />
+</td>
+ 
+<td width="25%">
+<b>Teen Book Club</b>
+<a href="http://www.emailbookclub.com/book/teen.htm"><img src="http://www.emailbookclub.com/book/teen.jpg" border="0" alt="Book Cover" width="94" height="145" /></a>
+<br />
+<a href="http://www.emailbookclub.com/book/teen.htm">About this book</a><br /><br />
+</td>
+</tr></table>
