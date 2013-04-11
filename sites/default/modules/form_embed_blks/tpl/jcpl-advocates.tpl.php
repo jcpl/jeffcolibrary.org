7c8b4059d2a027c65df17242f0058f15aa1447d8
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-11 17:06:15 -0600

    Update to iContact manual form.

--- sites/default/modules/form_embed_blks/tpl/jcpl-advocates.tpl.php
+++ sites/default/modules/form_embed_blks/tpl/jcpl-advocates.tpl.php
@@ -1,7 +1,8 @@
-<div><style type="text/css">
+<div><style>
 .link,
 .link a,
-#SignUp .signupframe {
+.signupframe
+{
 	color: #226699;
 	font-family: Arial, Helvetica, sans-serif;
 	font-size: 13px;
@@ -10,13 +11,101 @@
 	.link a {
 		text-decoration: none;
 		}
-	#SignUp .signupframe {
+	.signupframe {
 		border: 1px solid #000000;
 		background: #ffffff;
 		}
-#SignUp .signupframe .required {
+.signupframe .required {
 	font-size: 10px;
 	}
 </style>
-<script type="text/javascript" src="http://app.icontact.com/icp/loadsignup.php/form.js?c=1282908&l=3485&f=1615"></script>
+<form method="post" action="https://app.icontact.com/icp/signup.php" name="icpsignup" id="icpsignup1615" accept-charset="UTF-8" onsubmit="return verifyRequired1615();" >
+<input type="hidden" name="redirect" value="http://www.icontact.com/www/signup/thanks.html">
+<input type="hidden" name="errorredirect" value="http://www.icontact.com/www/signup/error.html">
+
+<div id="SignUp">
+<table width="260" class="signupframe" border="0" cellspacing="0" cellpadding="5">
+	<tr>
+      <td valign="top" align="right">
+        <span class="required">*</span> Email
+      </td>
+      <td align="left">
+        <input type="text" name="fields_email">
+      </td>
+    </tr>
+	<tr>
+      <td valign="top" align="right">
+        <span class="required">*</span> First Name
+      </td>
+      <td align="left">
+        <input type="text" name="fields_fname">
+      </td>
+    </tr>
+	<tr>
+      <td valign="top" align="right">
+        <span class="required">*</span> Last Name
+      </td>
+      <td align="left">
+        <input type="text" name="fields_lname">
+      </td>
+    </tr>
+	<!--<tr>
+      <td valign="top" align="right">
+        <span class="required">*</span> Business
+      </td>
+      <td align="left">
+        <input type="text" name="fields_business">
+      </td>
+    </tr> -->
+    <input type="hidden" name="listid" value="22584">
+    <input type="hidden" name="specialid:22584" value="XLJT">
+
+    <input type="hidden" name="clientid" value="1282908">
+    <input type="hidden" name="formid" value="1615">
+    <input type="hidden" name="reallistid" value="1">
+    <input type="hidden" name="doubleopt" value="0">
+    <!--<tr>
+      <td> </td>
+      <td><span class="required">*</span> = Required Field</td>
+    </tr>
+    <tr> -->
+      <td> </td>
+      <td><input type="submit" name="Submit" value="Submit"></td>
+    </tr>
+    </table>
+</div>
+</form>
+<script type="text/javascript">
+
+var icpForm1615 = document.getElementById('icpsignup1615');
+
+if (document.location.protocol === "https:")
+
+	icpForm1615.action = "https://app.icontact.com/icp/signup.php";
+function verifyRequired1615() {
+  if (icpForm1615["fields_email"].value == "") {
+    icpForm1615["fields_email"].focus();
+    alert("The Email field is required.");
+    return false;
+  }
+  if (icpForm1615["fields_fname"].value == "") {
+    icpForm1615["fields_fname"].focus();
+    alert("The First Name field is required.");
+    return false;
+  }
+  if (icpForm1615["fields_lname"].value == "") {
+    icpForm1615["fields_lname"].focus();
+    alert("The Last Name field is required.");
+    return false;
+  }
+  if (icpForm1615["fields_business"].value == "") {
+    icpForm1615["fields_business"].focus();
+    alert("The Business field is required.");
+    return false;
+  }
+
+
+return true;
+}
+</script>
 </div>
