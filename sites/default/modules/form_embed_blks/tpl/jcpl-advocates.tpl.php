11bafcfba8c8c80423beda9aebba17896a801b2a
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-04-11 14:33:26 -0600

    New form for iContact email sign up in form_embed_blks.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-advocates.tpl.php
@@ -0,0 +1,22 @@
+<div><style type="text/css">
+.link,
+.link a,
+#SignUp .signupframe {
+	color: #226699;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 13px;
+	}
+	.link,
+	.link a {
+		text-decoration: none;
+		}
+	#SignUp .signupframe {
+		border: 1px solid #000000;
+		background: #ffffff;
+		}
+#SignUp .signupframe .required {
+	font-size: 10px;
+	}
+</style>
+<script type="text/javascript" src="http://app.icontact.com/icp/loadsignup.php/form.js?c=1282908&l=3485&f=1615"></script>
+</div>
