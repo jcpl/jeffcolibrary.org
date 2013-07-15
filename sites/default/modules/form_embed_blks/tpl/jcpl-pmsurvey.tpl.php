d876a7949c8d3a9ffc75c036c1290a10bf11e0dc
Deborah Hamel <deborah.hamel@jeffcolibrary.org>
2013-07-15 15:03:41 -0600

    PM Survey form_embed_blks

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-pmsurvey.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form1901011107" id="formAnchor1901011107"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?1901011107"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV0mJhETqXc9p&1901011107",
+	width: "100%"
+});
+</script>
+</div>
