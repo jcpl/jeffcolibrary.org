2b7ab15a5f13142370834ab20dfb65d2fcbae186
root <root@ip-10-244-129-34.us-west-2.compute.internal>
2014-02-10 18:48:42 +0000

    Hot edit to form_embed_blks to add Book a Librarian input form.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-book-librarian.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form538217275" id="formAnchor538217275"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?538217275"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV3GRAHqZYlV%2F&538217275",
+	width: "100%"
+});
+</script>
+</div>
