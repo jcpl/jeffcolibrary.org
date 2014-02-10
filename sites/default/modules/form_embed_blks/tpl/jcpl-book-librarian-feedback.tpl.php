13666e6e224fa72d40e4c83947896d7acba901be
root <root@ip-10-244-129-34.us-west-2.compute.internal>
2014-02-10 18:46:55 +0000

    Hot edit to form_embed_blks to addfeedback form for Book a Librarian.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-book-librarian-feedback.tpl.php
@@ -0,0 +1,10 @@
+<div>
+<a name="form1079002627" id="formAnchor1079002627"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?1079002627"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscVwP1EGiTd%2FUr&1079002627",
+	width: "100%"
+});
+</script>
+</div>
