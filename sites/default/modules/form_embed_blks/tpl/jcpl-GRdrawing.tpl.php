e939e3223d276fd8c339a4387138bb6d0282e123
root <root@ip-10-220-131-69.us-west-2.compute.internal>
2014-06-23 21:11:04 +0000

    New form embed for dinner drawing for Golden Reads.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-GRdrawing.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form1127419325" id="formAnchor1127419325"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?1127419325"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV4hCSS9Y1D75&1127419325",
+	width: "100%"
+});
+</script>
+</div>
