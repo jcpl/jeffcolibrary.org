6e3a53f736ff0fb4a5e07b91418194f1574e90a6
root <root@ip-10-220-131-69.us-west-2.compute.internal>
2014-06-23 21:13:09 +0000

    New form embed for tickets to Golden Reads Amy Greene event.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-GRtickets.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form935910974" id="formAnchor935910974"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?935910974"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscV9RN2lSBrVcf&935910974",
+	width: "100%"
+});
+</script>
+</div>
