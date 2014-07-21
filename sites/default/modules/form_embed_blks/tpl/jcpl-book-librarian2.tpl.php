dd447595a66644705f4cf0d3a6ee6f4ae4a93201
root <root@ip-10-220-131-69.us-west-2.compute.internal>
2014-07-21 20:01:59 +0000

    New tpl created for live DRAFT of new form updates for BAL page.

---
+++ sites/default/modules/form_embed_blks/tpl/jcpl-book-librarian2.tpl.php
@@ -0,0 +1,9 @@
+<div><a name="form1335339801" id="formAnchor1335339801"></a>
+<script type="text/javascript" src="http://www.formsite.com/include/form/embedManager.js?1335339801"></script>
+<script type="text/javascript">
+EmbedManager.embed({
+	key: "http://www.formsite.com/res/showFormEmbed?EParam=3q7b%2B6hH5S2vCcHBnXscVzmzZM5HOf%2Fu&1335339801",
+	width: "100%"
+});
+</script>
+</div>
